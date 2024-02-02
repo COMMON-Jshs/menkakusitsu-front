66b916a4d31c127313c4de531bb48b9d0e52c4e0
vriskaserket51 <serketvriska51@gmail.com>
2024-02-03 06:33:44 +0900

    split firebase component and function

---
+++ src/components/Firebase.tsx
@@ -0,0 +1,38 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics, logEvent } from "firebase/analytics";
+import {
+  deleteToken,
+  getMessaging,
+  getToken,
+  onMessage,
+  Messaging,
+} from "firebase/messaging";
+import { getDeviceUuid } from "../utils/StorageManager";
+import { useEffect, useState } from "react";
+import { useSnackbar } from "notistack";
+import { DefaultSnackbar } from "./snackbar";
+import { putUserPush, deleteUserPush } from "../utils/Api";
+import { openConfirmDialog } from "./popup";
+import { DialogTitle } from "../utils/Constant";
+import { getFirebaseMessaging } from "../utils/FirebaseManager";
+
+function Firebase() {
+  const [messaging] = useState(getFirebaseMessaging());
+  const { enqueueSnackbar } = useSnackbar();
+
+  useEffect(() => {
+    if (messaging) {
+      onMessage(messaging, (payload) => {
+        enqueueSnackbar("fcm", {
+          variant: "default",
+          content: (key) => <DefaultSnackbar id={key} payload={payload} />,
+        });
+      });
+    }
+  }, [messaging, enqueueSnackbar]);
+
+  return <></>;
+}
+
+export default Firebase;
