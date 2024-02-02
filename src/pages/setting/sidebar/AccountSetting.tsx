b922d9909974001b2fba4b313ccbadb9dd77675a
vriskaserket51 <serketvriska51@gmail.com>
2024-02-03 06:21:56 +0900

    pop out to function

--- src/pages/setting/sidebar/AccountSetting.tsx
+++ src/pages/setting/sidebar/AccountSetting.tsx
@@ -15,8 +15,9 @@ import {
 } from "../../../utils/Api";
 import { DialogTitle } from "../../../utils/Constant";
 
-function AccountSetting() {
+function ChangeEmail() {
   const [email, setEmail] = useState<string | null>(null);
+  const [errorText, setErrorText] = useState<string | null>(null);
 
   useEffect(() => {
     getMyPrivateInfo({}).then((result) => {
@@ -24,149 +25,147 @@ function AccountSetting() {
     });
   }, []);
 
-  const drawChangeEmail = useCallback(() => {
-    const [errorText, setErrorText] = useState<string | null>(null);
-
-    return (
-      <Box
-        component="form"
-        onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
-          event.preventDefault();
-          setErrorText(null);
-          const data = new FormData(event.currentTarget);
-          const newEmail = data.get("email")?.toString();
-          if (email === null || !newEmail) {
-            return;
+  return (
+    <Box
+      component="form"
+      onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setErrorText(null);
+        const data = new FormData(event.currentTarget);
+        const newEmail = data.get("email")?.toString();
+        if (email === null || !newEmail) {
+          return;
+        }
+        if (email == newEmail) {
+          setErrorText("이건 당신이 이미 쓰고 계시는 이메일입니다.");
+          return;
+        }
+        if (!validateEmail(newEmail)) {
+          setErrorText("유효하지 않은 이메일입니다!");
+          return;
+        }
+        openWaitDialog(DialogTitle.Info, "잠시만 기다려주세요...");
+        putMyEmail({ oldEmail: email, newEmail: newEmail }).then((result) => {
+          if (isApiSuccessed(result)) {
+            closeWaitDialog();
+            setEmail(result.newEmail);
+          } else {
+            closeWaitDialog();
+            openConfirmDialog(DialogTitle.Info, result.message);
           }
-          if (email == newEmail) {
-            setErrorText("이건 당신이 이미 쓰고 계시는 이메일입니다.");
-            return;
-          }
-          if (!validateEmail(newEmail)) {
-            setErrorText("유효하지 않은 이메일입니다!");
-            return;
-          }
-          openWaitDialog(DialogTitle.Info, "잠시만 기다려주세요...");
-          putMyEmail({ oldEmail: email, newEmail: newEmail }).then((result) => {
-            if (isApiSuccessed(result)) {
-              closeWaitDialog();
-              setEmail(result.newEmail);
-            } else {
-              closeWaitDialog();
-              openConfirmDialog(DialogTitle.Info, result.message);
-            }
-          });
-        }}
-        sx={{ width: "50%" }}
-      >
-        <Typography variant="h5">이메일 변경</Typography>
-        <br />
-        {email !== null && (
-          <TextField
-            size="small"
-            label="이메일"
-            name="email"
-            error={Boolean(errorText)}
-            helperText={errorText}
-            defaultValue={email}
-            fullWidth
-            required
-          />
-        )}
-        <br />
-        <br />
-        <SubmitButton color="primary.main" width="25%" height="40px">
-          변경
-        </SubmitButton>
-      </Box>
-    );
-  }, [email]);
-
-  const drawChangePassword = useCallback(() => {
-    const [errorText, setErrorText] = useState<string | null>(null);
-
-    return (
-      <Box
-        component="form"
-        onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
-          event.preventDefault();
-          setErrorText(null);
-          const data = new FormData(event.currentTarget);
-          const oldPassword = data.get("oldPassword")?.toString();
-          const newPassword = data.get("newPassword")?.toString();
-          const newPasswordRe = data.get("newPasswordRe")?.toString();
-          if (!oldPassword || !newPassword || !newPasswordRe) {
-            return;
-          }
-          if (newPassword != newPasswordRe) {
-            setErrorText("비밀번호와 비밀번호 다시 입력이 다릅니다.");
-            return;
-          }
-          openWaitDialog(DialogTitle.Info, "잠시만 기다려주세요...");
-          putMyPassword({
-            oldPassword: Sha3(oldPassword),
-            newPassword: Sha3(newPassword),
-          }).then((result) => {
-            if (isApiSuccessed(result)) {
-              closeWaitDialog();
-              window.location.reload();
-            } else {
-              closeWaitDialog();
-              openConfirmDialog(DialogTitle.Info, result.message);
-            }
-          });
-        }}
-        sx={{ width: "50%" }}
-      >
-        <Typography variant="h5">비밀번호 변경</Typography>
-        <br />
+        });
+      }}
+      sx={{ width: "50%" }}
+    >
+      <Typography variant="h5">이메일 변경</Typography>
+      <br />
+      {email !== null && (
         <TextField
           size="small"
-          label="이전 비밀번호"
-          name="oldPassword"
-          type="password"
-          fullWidth
-          required
-        />
-        <br />
-        <br />
-        <TextField
-          size="small"
-          label="새 비밀번호"
-          name="newPassword"
-          type="password"
+          label="이메일"
+          name="email"
           error={Boolean(errorText)}
           helperText={errorText}
+          defaultValue={email}
           fullWidth
           required
         />
-        <br />
-        <br />
-        <TextField
-          size="small"
-          label="새 비밀번호 다시 입력"
-          name="newPasswordRe"
-          type="password"
-          error={Boolean(errorText)}
-          helperText={errorText}
-          fullWidth
-          required
-        />
-        <br />
-        <br />
-        <SubmitButton color="primary.main" width="25%" height="40px">
-          변경
-        </SubmitButton>
-      </Box>
-    );
-  }, [email]);
+      )}
+      <br />
+      <br />
+      <SubmitButton color="primary.main" width="25%" height="40px">
+        변경
+      </SubmitButton>
+    </Box>
+  );
+}
+
+function ChangePassword() {
+  const [errorText, setErrorText] = useState<string | null>(null);
 
   return (
+    <Box
+      component="form"
+      onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setErrorText(null);
+        const data = new FormData(event.currentTarget);
+        const oldPassword = data.get("oldPassword")?.toString();
+        const newPassword = data.get("newPassword")?.toString();
+        const newPasswordRe = data.get("newPasswordRe")?.toString();
+        if (!oldPassword || !newPassword || !newPasswordRe) {
+          return;
+        }
+        if (newPassword != newPasswordRe) {
+          setErrorText("비밀번호와 비밀번호 다시 입력이 다릅니다.");
+          return;
+        }
+        openWaitDialog(DialogTitle.Info, "잠시만 기다려주세요...");
+        putMyPassword({
+          oldPassword: Sha3(oldPassword),
+          newPassword: Sha3(newPassword),
+        }).then((result) => {
+          if (isApiSuccessed(result)) {
+            closeWaitDialog();
+            window.location.reload();
+          } else {
+            closeWaitDialog();
+            openConfirmDialog(DialogTitle.Info, result.message);
+          }
+        });
+      }}
+      sx={{ width: "50%" }}
+    >
+      <Typography variant="h5">비밀번호 변경</Typography>
+      <br />
+      <TextField
+        size="small"
+        label="이전 비밀번호"
+        name="oldPassword"
+        type="password"
+        fullWidth
+        required
+      />
+      <br />
+      <br />
+      <TextField
+        size="small"
+        label="새 비밀번호"
+        name="newPassword"
+        type="password"
+        error={Boolean(errorText)}
+        helperText={errorText}
+        fullWidth
+        required
+      />
+      <br />
+      <br />
+      <TextField
+        size="small"
+        label="새 비밀번호 다시 입력"
+        name="newPasswordRe"
+        type="password"
+        error={Boolean(errorText)}
+        helperText={errorText}
+        fullWidth
+        required
+      />
+      <br />
+      <br />
+      <SubmitButton color="primary.main" width="25%" height="40px">
+        변경
+      </SubmitButton>
+    </Box>
+  );
+}
+
+function AccountSetting() {
+  return (
     <>
-      {drawChangeEmail()}
+      <ChangeEmail />
       <br />
       <br />
-      {drawChangePassword()}
+      {<ChangePassword />}
       <br />
       <br />
     </>
