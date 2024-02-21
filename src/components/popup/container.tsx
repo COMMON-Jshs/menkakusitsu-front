d3d29b34899778f72f18ed706c36505948425d87
vriskaserket51 <serketvriska51@gmail.com>
2024-02-21 18:35:19 +0900

    close modals when payload change

--- src/components/popup/container.tsx
+++ src/components/popup/container.tsx
@@ -3,15 +3,17 @@ import { useLocation } from "react-router-dom";
 
 import { closeAll, useDialogContainerStore } from "@/components/popup/hooks";
 import { DialogComponent } from "@/components/popup/dialog";
+import { useAuth } from "@/hooks/useAuth";
 
 export function DialogContainer() {
   const { dialogs } = useDialogContainerStore();
 
   const location = useLocation();
+  const { payload } = useAuth();
 
   useEffect(() => {
     closeAll();
-  }, [location]);
+  }, [location, payload]);
 
   return (
     <>
