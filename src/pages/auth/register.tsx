0f82e2a9752ceb004369f17c760da88b5e9d46db
vriskaserket51 <serketvriska51@gmail.com>
2024-02-21 22:14:13 +0900

    add auth page

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import RegisterPanel from "@/components/panels/RegisterPanel";
 import { setHeaderActive } from "@/hooks/useNavbar";
 
-export function Register() {
+export default function RegisterScreen() {
   useEffect(() => {
     setHeaderActive(false);
     return () => {
