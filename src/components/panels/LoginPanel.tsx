24b901433d3d57f4f52eb44bf5c63e7c254cc003
vriskaserket51 <serketvriska51@gmail.com>
2024-02-22 17:04:25 +0900

    make register panel popup

--- src/components/panels/LoginPanel.tsx
+++ src/components/panels/LoginPanel.tsx
@@ -5,67 +5,70 @@ import { AccountBox } from "@mui/icons-material";
 import { v1 } from "@common-jshs/menkakusitsu-lib";
 
 import Popup from "@/components/popup";
-import { IconNavLink, TitleText } from "@/components/basics";
+import { SubmitButton, Text, TitleText } from "@/components/basics";
 import { Api, Constants, Firebase, Storage, Utility } from "@/utils";
 import { login } from "@/hooks/useAuth";
-import { redirect } from "@/router";
-
-const onPostLogin = (event: React.MouseEvent<HTMLFormElement>) => {
-  event.preventDefault();
-  const data = new FormData(event.currentTarget);
-  const id = data.get("id")?.toString();
-  const password = data.get("password")?.toString();
-  if (!id || !password) {
-    return;
-  }
-
-  Popup.startLoading("로그인 중입니다...");
-  Api.postLogin({ id: id, password: Utility.Sha3(password) }).then((result) => {
-    Popup.stopLoading();
-    if (Api.isSuccessed(result)) {
-      onLoginSuccessed(result);
-    } else {
-      Popup.openConfirmDialog(Constants.DialogTitle.Info, result.message);
-    }
-  });
-};
-
-const onLoginSuccessed = async (result: v1.PostLoginResponse) => {
-  Storage.saveTokens(result.accessToken, result.refreshToken);
-
-  if (Api.getPushApproved()) {
-    await Firebase.createPushToken();
-  }
-
-  login(result.accessToken);
-
-  if (result.callbacks) {
-    if (result.callbacks.includes("needChangePw")) {
-      Popup.openConfirmDialog(
-        Constants.DialogTitle.Alert,
-        "기존 4자리 학번을 비밀번호로 사용하시는 경우, 비밀번호를 바꾸셔야합니다.",
-        () => {
-          // redirect("/setting")
-        }
-      );
-      return;
-    }
-    if (result.callbacks.includes("needChangeEmail")) {
-      Popup.openConfirmDialog(
-        Constants.DialogTitle.Alert,
-        "비밀번호 복구 등의 서비스를 이용하시려면 이메일을 추가하셔야합니다.",
-        () => {
-          // redirect("/setting")
-        }
-      );
-      return;
-    }
-  }
-
-  redirect("/auth/register");
-};
+import { useNavigate } from "@/router";
+import RegisterPanel from "@/components/panels/RegisterPanel";
 
 export default function LoginPanel() {
+  const navigate = useNavigate();
+
+  const onPostLogin = (event: React.MouseEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const id = data.get("id")?.toString();
+    const password = data.get("password")?.toString();
+    if (!id || !password) {
+      return;
+    }
+
+    Popup.startLoading("로그인 중입니다...");
+    Api.postLogin({ id: id, password: Utility.Sha3(password) }).then(
+      (result) => {
+        Popup.stopLoading();
+        if (Api.isSuccessed(result)) {
+          onLoginSuccessed(result);
+        } else {
+          Popup.openConfirmDialog(Constants.DialogTitle.Info, result.message);
+        }
+      }
+    );
+  };
+
+  const onLoginSuccessed = async (result: v1.PostLoginResponse) => {
+    Storage.saveTokens(result.accessToken, result.refreshToken);
+
+    if (Api.getPushApproved()) {
+      await Firebase.createPushToken();
+    }
+
+    login(result.accessToken);
+
+    if (result.callbacks) {
+      if (result.callbacks.includes("needChangePw")) {
+        Popup.openConfirmDialog(
+          Constants.DialogTitle.Alert,
+          "기존 4자리 학번을 비밀번호로 사용하시는 경우, 비밀번호를 바꾸셔야합니다.",
+          () => {
+            navigate("/setting");
+          }
+        );
+        return;
+      }
+      if (result.callbacks.includes("needChangeEmail")) {
+        Popup.openConfirmDialog(
+          Constants.DialogTitle.Alert,
+          "비밀번호 복구 등의 서비스를 이용하시려면 이메일을 추가하셔야합니다.",
+          () => {
+            navigate("/setting");
+          }
+        );
+        return;
+      }
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -96,6 +99,7 @@ export default function LoginPanel() {
           id="id"
           label="ID"
           name="id"
+          autoComplete="username"
         />
         <TextField
           className="inputRounded"
@@ -105,24 +109,16 @@ export default function LoginPanel() {
           label="Password"
           type="password"
           id="password"
+          autoComplete="current-password"
         />
-        <Button
-          type="submit"
+        <SubmitButton
           fullWidth
-          variant="contained"
-          sx={{
-            mt: 3,
-            mb: 2,
-            borderRadius: "50px",
-            backgroundColor: "primary.main",
-            "&:hover": {
-              backgroundColor: "#fff",
-              color: "primary.main",
-            },
-          }}
+          backgroundColor="primary.main"
+          height="40px"
+          sx={{ mt: 3, mb: 2, borderRadius: "50px" }}
         >
           <TitleText>LOGIN</TitleText>
-        </Button>
+        </SubmitButton>
         <Stack
           spacing={1}
           direction={{ xs: "column", sm: "row" }}
@@ -142,11 +138,14 @@ export default function LoginPanel() {
           >
             비밀번호를 잊어버리셨나요?
           </Link>
-          <IconNavLink
-            to="/auth/register"
-            label="아직 계정이 없으신가요?"
-            icon={<AccountBox />}
-          />
+          <Button
+            onClick={() => {
+              Popup.openCancelableDialog("", <RegisterPanel />);
+            }}
+            startIcon={<AccountBox />}
+          >
+            <Text>아직 계정이 없으신가요?</Text>
+          </Button>
         </Stack>
       </Box>
     </Box>
--- src/components/panels/RegisterPanel.tsx
+++ src/components/panels/RegisterPanel.tsx
@@ -1,11 +1,10 @@
-import { Box, Container, Paper, TextField } from "@mui/material";
-import { Fragment } from "react";
-import { useNavigate } from "react-router-dom";
+import { Box, TextField } from "@mui/material";
 
 import Popup from "@/components/popup";
 import { TitleText } from "@/components/basics";
 import { SubmitButton } from "@/components/basics/StyledButton";
 import { Api, Constants, Utility } from "@/utils";
+import { useNavigate } from "@/router";
 
 export default function RegisterPanel() {
   const navigate = useNavigate();
@@ -19,7 +18,7 @@ export default function RegisterPanel() {
     const id = data.get("id")?.toString().trim();
     const password = data.get("password")?.toString().trim();
     const passwordCheck = data.get("passwordCheck")?.toString().trim();
-    if (Number.isNaN(sid)) {
+    if (Number.isNaN(sid) || !Number.isInteger(sid) || sid <= 0) {
       Popup.openConfirmDialog(
         Constants.DialogTitle.Info,
         "학번의 형식이 잘못되었습니다."
@@ -64,91 +63,95 @@ export default function RegisterPanel() {
   };
 
   return (
-    <Fragment>
-      <Container
-        maxWidth="md"
+    <Box
+      sx={{
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "16px",
+      }}
+    >
+      <TitleText
+        variant="h4"
         sx={{
-          margin: "30px auto 50px",
+          mr: 2,
+          color: "inherit",
+          textDecoration: "none",
         }}
       >
-        <Paper>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              flexWrap: "wrap",
-              padding: "50px 50px 30px 50px",
-            }}
-          >
-            <TitleText>회원가입</TitleText>
-            <Box
-              component="form"
-              onSubmit={onPostRegister}
-              sx={{ mt: 1, padding: "0 30px 0" }}
-            >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                size="small"
-                name="name"
-                label="이름"
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                size="small"
-                name="sid"
-                label="학번"
-                helperText="1학년 1반 1번이면 1101"
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                size="small"
-                name="email"
-                label="이메일"
-                type="email"
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                size="small"
-                name="id"
-                label="아이디"
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                size="small"
-                name="password"
-                label="비밀번호"
-                type="password"
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                size="small"
-                name="passwordCheck"
-                label="비밀번호 확인"
-                type="password"
-              />
-              <Box sx={{ marginTop: 3 }}>
-                <SubmitButton width="50%" backgroundColor="primary.main">
-                  가입하기
-                </SubmitButton>
-              </Box>
-            </Box>
-          </Box>
-        </Paper>
-      </Container>
-    </Fragment>
+        회원가입
+      </TitleText>
+      <Box
+        component="form"
+        onSubmit={onPostRegister}
+        sx={{ mt: 1, padding: "0 30px 0" }}
+      >
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          size="small"
+          name="name"
+          label="이름"
+          autoComplete="name"
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          size="small"
+          name="sid"
+          label="학번"
+          type="number"
+          sx={{ appearance: "none" }}
+          helperText="1학년 1반 1번이면 1101"
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          size="small"
+          name="email"
+          label="이메일"
+          type="email"
+          autoComplete="email"
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          size="small"
+          name="id"
+          label="아이디"
+          autoComplete="username"
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          size="small"
+          name="password"
+          label="비밀번호"
+          type="password"
+          autoComplete="current-password"
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          size="small"
+          name="passwordCheck"
+          label="비밀번호 확인"
+          type="password"
+        />
+        <Box sx={{ marginTop: 3 }}>
+          <SubmitButton width="192px" backgroundColor="primary.main">
+            가입하기
+          </SubmitButton>
+        </Box>
+      </Box>
+    </Box>
   );
 }
