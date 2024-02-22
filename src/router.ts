59c213e203cff2cb9e414511f0c6a71a4b54aa6f
vriskaserket51 <serketvriska51@gmail.com>
2024-02-22 17:03:47 +0900

    add custom route

--- src/router.ts
+++ src/router.ts
@@ -1,25 +1,44 @@
 // Generouted, changes to this file will be overriden
 /* eslint-disable */
 
+import { useGenroute } from '@/hooks/useGenroute';
 import { components, hooks, utils } from '@generouted/react-router/client'
 
 export type Path =
   | `/`
+  | `/attendance`
   | `/attendance/download`
   | `/attendance/info`
+  | `/auth`
   | `/auth/login`
   | `/auth/register`
+  | `/bbs`
+  | `/bbs/:board/:postId/edit`
+  | `/bbs/:board/:postId/view`
+  | `/bbs/:board/create`
+  | `/bbs/:board/list`
+  | `/chat`
+  | `/chat/idbot`
+  | `/construct`
   | `/contributors`
+  | `/dev`
+  | `/dev/user`
+  | `/setting`
+  | `/setting/account`
+  | `/specialroom`
   | `/specialroom/apply`
   | `/specialroom/management`
   | `/specialroom/status`
 
 export type Params = {
-  
+  '/bbs/:board/:postId/edit': { board: string; postId: string }
+  '/bbs/:board/:postId/view': { board: string; postId: string }
+  '/bbs/:board/create': { board: string }
+  '/bbs/:board/list': { board: string }
 }
 
 export type ModalPath = never
 
 export const { Link, Navigate } = components<Path, Params>()
-export const { useModals, useNavigate, useParams } = hooks<Path, Params, ModalPath>()
+export const { useModals, useNavigate, useParams } = useGenroute<Path, Params, ModalPath>()
 export const { redirect } = utils<Path, Params>()
