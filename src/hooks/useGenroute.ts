aba2282abd321ea6889e80877247d841113d4f61
vriskaserket51 <serketvriska51@gmail.com>
2024-02-22 14:45:19 +0900

    temp file

---
+++ src/hooks/useGenroute.ts
@@ -0,0 +1,85 @@
+import { useCallback, useMemo } from "react";
+import {
+  generatePath,
+  NavigateOptions,
+  useLocation,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
+
+export const useGenroute = <
+  Path extends string,
+  Params extends Record<string, any>,
+  ModalPath extends string
+>() => {
+  type ParamPath = keyof Params;
+  type To = { pathname: Path; search?: string; hash?: string };
+
+  type NavigateOptionsWithParams<P extends Path | To | number> =
+    P extends number
+      ? []
+      : P extends ParamPath
+      ? [NavigateOptions & { params: Params[P] }]
+      : P extends To
+      ? P["pathname"] extends ParamPath
+        ? [NavigateOptions & { params: Params[P["pathname"]] }]
+        : [NavigateOptions & { params?: never }]
+      : [NavigateOptions & { params?: never }] | [];
+
+  return {
+    useParams: <P extends ParamPath>(path: P) =>
+      useParams<Params[typeof path]>() as Params[P],
+    useNavigate: () => {
+      const navigate = useNavigate();
+      const newUseNavigate = useCallback(
+        <P extends Path | To | number>(
+          to: P,
+          ...[options]: NavigateOptionsWithParams<P>
+        ) => {
+          if (typeof to === "number") return navigate(to);
+          const path = generatePath(
+            typeof to === "string" ? to : to.pathname,
+            options?.params || ({} as any)
+          );
+          navigate(
+            typeof to === "string"
+              ? path
+              : { pathname: path, search: to.search, hash: to.hash },
+            options
+          );
+        },
+        [navigate]
+      );
+      return newUseNavigate;
+    },
+    useModals: () => {
+      const location = useLocation();
+      const navigate = useNavigate();
+
+      type Options<P> = NavigateOptions &
+        (P extends ParamPath
+          ? { at?: P; params: Params[P] }
+          : { at?: P; params?: never });
+
+      return useMemo(() => {
+        return {
+          current: location.state?.modal || "",
+          open: <P extends Path>(path: ModalPath, options?: Options<P>) => {
+            const { at, state, ...opts } = options || {};
+            navigate(at || location.pathname, {
+              ...opts,
+              state: { ...location.state, ...state, modal: path },
+            });
+          },
+          close: <P extends Path>(options?: Options<P>) => {
+            const { at, state, ...opts } = options || {};
+            navigate(at || location.pathname, {
+              ...opts,
+              state: { ...location.state, ...state, modal: "" },
+            });
+          },
+        };
+      }, [location, navigate]);
+    },
+  };
+};
