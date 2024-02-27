2c3408aa678b9f70a78001fca2f56da54db41fec
vriskaserket51 <serketvriska51@gmail.com>
2024-02-27 17:40:13 +0900

    update member info

--- src/pages/contributors/members.ts
+++ src/pages/contributors/members.ts
@@ -9,15 +9,15 @@ type Member = {
 const contributors: Member[] = [
   {
     name: "23기 고승한",
-    description: "뉴디저디 기획 및 프론트/백 엔드 개발을 했습니다.",
-    period: "2022 ~ 2023.03.05.",
+    description: "이디저디 2.0 기획 및 프론트/백 엔드 개발을 했습니다.",
+    period: "2022 ~ 2024.02.29.",
     profile: "https://avatars.githubusercontent.com/VriskaSerket51",
     homepage: "https://github.com/VriskaSerket51",
   },
   {
     name: "23기 선우준",
     description: "서버 컴퓨터 관리 및 OS 구성을 했습니다.",
-    period: "2022 ~ ",
+    period: "2022 ~ 2024.02.29.",
     profile: "https://avatars.githubusercontent.com/sunsh9876543210",
     homepage: "https://github.com/sunsh9876543210",
   },
