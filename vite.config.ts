e49279261d25d3ccf6ac6d16ffb37386c9d7f665
vriskaserket51 <serketvriska51@gmail.com>
2024-02-28 17:35:14 +0900

    add gzip pre-compression support

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { compression } from "vite-plugin-compression2";
 import generouted from "@generouted/react-router/plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), generouted()],
+  plugins: [react(), tsconfigPaths(), generouted(), compression()],
 });
